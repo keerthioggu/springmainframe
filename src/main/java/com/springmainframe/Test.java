48b23e96a5706edacccb68613a0af66ae1aa0603
user <user@192.168.0.19>
2019-06-11 17:13:20 +0530

    added sop

--- src/main/java/com/springmainframe/Test.java
+++ src/main/java/com/springmainframe/Test.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 		public String accessDenied() {
 			return "";
 		}
+		public static void main(String[] args) {
+			System.out.println("welcome");
+		}
 
 	}
 
